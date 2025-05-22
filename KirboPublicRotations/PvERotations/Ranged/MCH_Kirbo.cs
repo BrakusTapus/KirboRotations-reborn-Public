47bea7c6af4ccc9211187de55b3aec998f519045
BrakusTapus <x_aj1993@live.nl>
2025-05-22 18:25:43 +0200

    test

---
+++ KirboPublicRotations/PvERotations/Ranged/MCH_Kirbo.cs
@@ -0,0 +1,19 @@
+﻿namespace KirboPublicRotations.PvERotations.Ranged;
+
+[BetaRotation]
+[Rotation("Kirbo's MCH", CombatType.PvE, GameVersion = "7.21", Description = "")]
+[Api(4)]
+public class MchKirbo : MachinistRotation
+{
+    //GCD actions here.
+    protected override bool GeneralGCD(out IAction? act)
+    {
+        return base.GeneralGCD(out act);
+    }
+
+    //0GCD actions here.
+    protected override bool AttackAbility(IAction nextGCD, out IAction? act)
+    {
+        return base.AttackAbility(nextGCD, out act);
+    }
+}
