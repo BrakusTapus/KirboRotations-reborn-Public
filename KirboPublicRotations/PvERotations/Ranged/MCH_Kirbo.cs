f415e12e5b7c6599a8595bec31350f16a24813da
BrakusTapus <x_aj1993@live.nl>
2025-06-10 18:52:08 +0200

    update mch name

--- KirboPublicRotations/PvERotations/Ranged/MCH_Kirbo.cs
+++ KirboPublicRotations/PvERotations/Ranged/MCH_Kirbo.cs
@@ -1,10 +1,20 @@
 ﻿namespace KirboPublicRotations.PvERotations.Ranged;
 
 [BetaRotation]
-[Rotation("Kirbo's MCH", CombatType.PvE, GameVersion = "7.21", Description = "")]
+[Rotation("Kirbo's MCH\n [Public]", CombatType.PvE, GameVersion = "7.25", Description = "")]
 [Api(4)]
-public class MchKirbo : MachinistRotation
+public class MchKirboPublic : MachinistRotation
 {
+    #region Countdown Logic
+    // Override the method for actions to be taken during the countdown phase of combat
+    protected override IAction? CountDownAction(float remainTime)
+    {
+        if (remainTime > 5) return base.CountDownAction(remainTime);
+        if (remainTime < 5 && ReassemblePvE.CanUse(out var act)) return act;
+        return base.CountDownAction(remainTime);
+    }
+    #endregion
+
     //GCD actions here.
     protected override bool GeneralGCD(out IAction? act)
     {
@@ -15,5 +25,13 @@ public class MchKirbo : MachinistRotation
     protected override bool AttackAbility(IAction nextGCD, out IAction? act)
     {
         return base.AttackAbility(nextGCD, out act);
+    }
+
+    private bool ShouldUseReassemble(out IAction? act, IAction nextGCD)
+    {
+
+
+        act = null;
+        return false;
     }
 }
